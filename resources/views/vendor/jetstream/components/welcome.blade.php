4e5fa618e0e5079d7d2d8fd61ea51754f4a30f16
Pavel Zablockij <pavel.zablockij@vinted.com>
2022-09-25 16:02:00 +0300

    Update home page

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -8,33 +8,36 @@
     </div>
 
     <div class="mt-6 text-gray-500">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore reiciendis eius voluptatum veritatis, minus repellendus. Cupiditate voluptatem vero tempora dolor ea modi quaerat eveniet, obcaecati consectetur nostrum id quo excepturi.
+        The main idea would be to create a community that would be like a single and universal digital investigation tool for all researchers. This tool could have solve several problems at once and unite researchers into one joint team.
     </div>
 </div>
 
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
-    <div class="p-6">
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
-            </svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Guides</div>
-        </div>
-
-        <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem omnis veniam, molestiae praesentium commodi sit et culpa, facere explicabo voluptas eos ratione inventore in architecto dicta vero. Quibusdam, fugit enim.
+    <div class="relative">
+        <div class="absolute w-full h-full bg-gray-100 bg-opacity-60"></div>
+        <div class="p-6">
+            <div class="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
+                </svg>
+                <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Guides</div>
             </div>
 
-            <a href="#">
-                <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
-                        <div>Start learning</div>
-
-                        <div class="ml-1 text-indigo-500">
-                            <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </div>
+            <div class="ml-12">
+                <div class="mt-2 text-sm text-gray-500">
+                    It would be nice if people would be able to share their knowledge with others, ask for help or write down simple instructions about different methods and techniques on how to solve various problems and find the information researchers are looking for.
                 </div>
-            </a>
+
+                <a href="#">
+                    <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
+                            <div>Start learning</div>
+
+                            <div class="ml-1 text-indigo-500">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </div>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 
@@ -48,7 +51,7 @@
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, facere impedit ut aspernatur autem earum recusandae numquam iusto illo nulla harum assumenda. Explicabo, sint voluptate doloremque fugiat sequi excepturi ipsum.
+                Suggest or request new tools, post guides on how to use these tools. Explore tools using based on topics, the data you have or looking for, save your favorites tools and access them easily in your profile. Attach tools you have been using for your research, help other researchers to find the right tools. In the future, Tools database could be used for building flow charts or OSINT frameworks. Tools could be integrated using APIs in order to provide user-friendly UI and easy access for everyone.
             </div>
 
             <a href="{{ route('tools.index') }}">
@@ -63,53 +66,59 @@
         </div>
     </div>
 
-    <div class="p-6 border-t border-gray-200">
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
-            </svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Community</div>
-        </div>
-
-        <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui reiciendis culpa explicabo amet nisi perferendis debitis expedita enim placeat, illum ipsa ut consequatur officiis aliquid vitae magni, molestias porro rerum.
+    <div class="relative border-t border-gray-200">
+        <div class="absolute w-full h-full bg-gray-100 bg-opacity-60"></div>
+        <div class="p-6">
+            <div class="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                </svg>
+                <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Community</div>
             </div>
 
-            <a href="#">
-                <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
-                        <div>Find researchers</div>
-
-                        <div class="ml-1 text-indigo-500">
-                            <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </div>
+            <div class="ml-12">
+                <div class="mt-2 text-sm text-gray-500">
+                    Introduce yourself, highlight your strongest areas, discover and find co-researchers, participate in research challenges or competitions, share your tooling or work, build OSINT frameworks, collaborate. In my opinion, it should be a closed community, maybe Bellingcat or community itself could verify and approve people who want to join.
                 </div>
-            </a>
+
+                <a href="#">
+                    <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
+                            <div>Find researchers</div>
+
+                            <div class="ml-1 text-indigo-500">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </div>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 
-    <div class="p-6 border-t border-gray-200 md:border-l">
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-            </svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Investigation</div>
-        </div>
-
-        <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti reprehenderit eos quae non quis cupiditate vel quasi labore nam provident quia ratione quibusdam iste soluta, magnam aut quas rem consequuntur.
+    <div class="relative border-t border-gray-200 md:border-l">
+        <div class="absolute w-full h-full bg-gray-100 bg-opacity-60"></div>
+        <div class="p-6">
+            <div class="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                </svg>
+                <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Investigation</div>
             </div>
 
-            <a href="#">
-                <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
-                        <div>Contribute</div>
-
-                        <div class="ml-1 text-indigo-500">
-                            <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </div>
+            <div class="ml-12">
+                <div class="mt-2 text-sm text-gray-500">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti reprehenderit eos quae non quis cupiditate vel quasi labore nam provident quia ratione quibusdam iste soluta, magnam aut quas rem consequuntur.
                 </div>
-            </a>
+
+                <a href="#">
+                    <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
+                            <div>Contribute</div>
+
+                            <div class="ml-1 text-indigo-500">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </div>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </div>
